94d2ed17a930ee1b9e2e937c58c1d7f2f71a1b87
Vincent Battaglia <vpbattaglia@gmail.com>
2020-04-25 07:25:04 -0700

    Level 3 - Salting and Hashing Passwords with bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -43,10 +43,14 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+
 
   newUser.save(function(err){
     if (err) {
@@ -56,25 +60,29 @@ app.post("/register", function(req, res){
     }
   });
 });
-
+});
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+        });
+
         }
       }
-   }
+   });
 });
 
-});
+
 
 
 app.listen(3000, function() {
