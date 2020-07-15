f747fd3fda05b0071496ebf2028c91d7d1d1f33e
Vincent Battaglia <vpbattaglia@gmail.com>
2020-07-14 18:44:54 -0700

    test

--- app.js
+++ app.js
@@ -203,6 +203,4 @@ app.post("/login", function(req, res){
 
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000.");
-});
+app.listen(process.env.PORT || 3000)
